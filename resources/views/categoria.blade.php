ee0e49835c8ce7ab7445a84203ccc52d6c06f829
Maximillian Fernandez <dgse.informatica@gmail.com>
2020-05-11 12:29:29 -0500

    categoria

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -4,7 +4,6 @@
  <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
   <title>
     Favores
   </title>
